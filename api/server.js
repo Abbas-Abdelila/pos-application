b1f6d6b5e5190a5ddf22b81f19492cc9244ae387
Abbas-Abdelila <abbasabdelila@gmail.com>
2023-02-14 18:01:15 +0300

    port change

--- api/server.js
+++ api/server.js
@@ -31,7 +31,9 @@ const connect = async () => {
 
 // Middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: ["http://localhost:5000", "https://payd-pos-app.onrender.com"]
+}))
 app.use(logger("dev"))
 
 app.use("/api/categories", categoryRoute);
